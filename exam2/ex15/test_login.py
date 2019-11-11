848c3d20e6581d9cce3e0c71a162b303ec6d6fbc
Mehbarki <mehdi-mebarki@live.fr>
2019-11-11 11:52:17 +0100

    update exos

--- exam2/ex15/test_login.py
+++ exam2/ex15/test_login.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+
+from login import compare_pass
+from sys import argv
+
+if len(argv) > 1:
+    if compare_pass(argv[1]):
+        print("login ok")
+    else:
+        print("login failed")
